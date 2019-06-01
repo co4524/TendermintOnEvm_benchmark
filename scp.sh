2ff8261bff6d2b1152ebc9339ae3ae0f5787249c
co4524 <monster3697@gmail.com>
2019-06-01 09:54:03 +0000

    scp_v2

--- scp.sh
+++ scp.sh
@@ -1,8 +1,8 @@
 remote_path=/home/caideyi/TendermintOnEvm_benchmark/data
 data_path1=$HOME/TendermintOnEvm_benchmark/data/blockCommitTime.txt
 data_path2=$HOME/TendermintOnEvm_benchmark/data/blockTxNum.txt
-gcloud compute scp workloadgenerator:$remote_path $data_path1 --zone 
-gcloud compute scp workloadgenerator:$remote_path $data_path1 --zone 
+gcloud compute scp $data_path1 workloadgenerator:$remote_path --zone asia-east1-b
+gcloud compute scp $data_path2 workloadgenerator:$remote_path --zone asia-east1-b
 rm $data_path1
 rm $data_path2
 touch $data_path1
