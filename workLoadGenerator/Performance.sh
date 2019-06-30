817592b410504cf16db9b5af2e641033e7b276ef
co4524 <monster3697@gmail.com>
2019-07-01 02:13:48 +0800

    txrate speed up

--- workLoadGenerator/Performance.sh
+++ workLoadGenerator/Performance.sh
@@ -21,7 +21,13 @@ path_avg_latency=$path2/latency
 path_avg_txRate=$path2/txRate
 path_avg_fail=$path2/fail
 path_workload=$HOME/evm-lite-js/test/workload.js
+path_genRaw=$HOME/evm-lite-js/test/genRaw.js
 path_cal=$HOME/evm-lite-js/test/cal.py
+path_checksum=$HOME/evm-lite-js/test/checkSum.js
+path_checkbalance=$HOME/evm-lite-js/test/checkBalance.js
+
+threadNum=6
+
 
 
 ResetLogFile(){
@@ -38,7 +44,7 @@ Reset(){
 }
 
 WorkLoad(){
-	node $path_workload $1 $2 
+	node $path_workload $1 $2 $3
 }
 
 SCP_instance(){
@@ -50,12 +56,23 @@ SCP_instance(){
 
 main(){
 
+	let RawTxNum=$2*threadNum
 	echo "-------Start Testing--------"
 	for ((j=0 ; j<$3 ; j++)){
+		echo "Testing interArrival Time : $1 totalTxNum : $2"
+		sc=0  #start account
 		ResetLogFile
-		WorkLoad $1 $2
-	        echo "Transfer data....."	
+		init=$(node $path_checkbalance)
+		node $path_genRaw $RawTxNum
 		sleep 5
+		echo "Sending transaction ......"
+		for ((i=1 ; i< $threadNum ; i++)) {
+			WorkLoad $1 $2 $sc &
+			let sc=sc+$2
+		}
+		WorkLoad $1 $2 $sc 
+		node $path_checksum $2 $init
+	    echo "Transfer data....."	
 		#SCP_instance $4
 		echo "CalPerformance....."
 		python $path_cal 
@@ -65,4 +82,5 @@ main(){
 
 Reset
 main $1 $2 $3 $4    ##[1] sleep time : ms  [2] tx_num  [3]: iter [4]: instance name
+echo "Evaluate Variance&Mean..."
 python variance.py
